9d74b3458d1e7ba0087de063805c73511291302f
Alex Malins <github@alexmalins.com>
2020-02-08 13:47:44 +0900

    Translations ch02 03->05
    Committed by mistake to master branch before

--- ch02/ch02-04-optimize-cv.py
+++ ch02/ch02-04-optimize-cv.py
@@ -2,21 +2,21 @@ import numpy as np
 import pandas as pd
 
 # -----------------------------------
-# out-of-foldでの閾値の最適化
+# Optimization of out-of-fold threshold
 # -----------------------------------
 from scipy.optimize import minimize
 from sklearn.metrics import f1_score
 from sklearn.model_selection import KFold
 
-# サンプルデータ生成の準備
+# Prepartion for sample data creation
 rand = np.random.RandomState(seed=71)
 train_y_prob = np.linspace(0, 1.0, 10000)
 
-# 真の値と予測値が以下のtrain_y, train_pred_probであったとする
+# Assume that the true and predicted values are train_y and train_pred_prob, respectively
 train_y = pd.Series(rand.uniform(0.0, 1.0, train_y_prob.size) < train_y_prob)
 train_pred_prob = np.clip(train_y_prob * np.exp(rand.standard_normal(train_y_prob.shape) * 0.3), 0.0, 1.0)
 
-# クロスバリデーションの枠組みで閾値を求める
+# Find thresholds using cross-validation framework
 thresholds = []
 scores_tr = []
 scores_va = []
@@ -26,11 +26,11 @@ for i, (tr_idx, va_idx) in enumerate(kf.split(train_pred_prob)):
     tr_pred_prob, va_pred_prob = train_pred_prob[tr_idx], train_pred_prob[va_idx]
     tr_y, va_y = train_y.iloc[tr_idx], train_y.iloc[va_idx]
 
-    # 最適化の目的関数を設定
+    # Objective function for optimization
     def f1_opt(x):
         return -f1_score(tr_y, tr_pred_prob >= x)
 
-    # 学習データで閾値の最適化を行い、バリデーションデータで評価を行う
+    # Optimize thresholds with training data, and evaluate with validation data
     result = minimize(f1_opt, x0=np.array([0.5]), method='Nelder-Mead')
     threshold = result['x'].item()
     score_tr = f1_score(tr_y, tr_pred_prob >= threshold)
@@ -41,6 +41,6 @@ for i, (tr_idx, va_idx) in enumerate(kf.split(train_pred_prob)):
     scores_tr.append(score_tr)
     scores_va.append(score_va)
 
-# 各foldの閾値の平均をテストデータには適用する
+# Apply mean of the fold thresholds to the test data 
 threshold_test = np.mean(thresholds)
 print(threshold_test)
