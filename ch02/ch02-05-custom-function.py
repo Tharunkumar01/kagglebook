138efbbf96834e4c2dcded558d88c1d979b9e2e7
Alex Malins <github@alexmalins.com>
2020-02-08 13:43:15 +0900

    Revert "translate last two scripts of ch02"
    This reverts commit b7967a218774c76c2c3430b7dbe569a6a801536b.

--- ch02/ch02-05-custom-function.py
+++ ch02/ch02-05-custom-function.py
@@ -3,25 +3,25 @@ import pandas as pd
 
 
 # -----------------------------------
-# Optimizing MAE by approximating metric with custom objective function
+# カスタム目的関数での評価指標の近似によるMAEの最適化
 # -----------------------------------
 
-# Fair function
+# Fair 関数
 def fair(preds, dtrain):
-    x = preds - dtrain.get_labels()  # Get residual
-    c = 1.0  # Parameter of fair function
-    den = abs(x) + c  # Calculate denominator of gradient formula
-    grad = c * x / den  # Gradient
-    hess = c * c / den ** 2  # Second derivative
+    x = preds - dtrain.get_labels()  # 残差を取得
+    c = 1.0  # Fair関数のパラメータ
+    den = abs(x) + c  # 勾配の式の分母を計算
+    grad = c * x / den  # 勾配
+    hess = c * c / den ** 2  # 二階微分値
     return grad, hess
 
 
-# Pseudo-Huber function
+# Pseudo-Huber 関数
 def psuedo_huber(preds, dtrain):
-    d = preds - dtrain.get_labels()  # Get residual
-    delta = 1.0  # Parameter of Pseudo-Huber function
+    d = preds - dtrain.get_labels()  # 残差を取得
+    delta = 1.0  # Pseudo-Huber関数のパラメータ
     scale = 1 + (d / delta) ** 2
     scale_sqrt = np.sqrt(scale)
-    grad = d / scale_sqrt  # Gradient
-    hess = 1 / scale / scale_sqrt  # Second derivative
+    grad = d / scale_sqrt  # 勾配
+    hess = 1 / scale / scale_sqrt  # 二階微分値
     return grad, hess
