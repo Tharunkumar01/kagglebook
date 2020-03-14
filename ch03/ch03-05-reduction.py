1c1c3553a29d33ed5136c9f51b73a82b7f08254f
Alex Malins <github@alexmalins.com>
2020-03-14 14:51:21 +0900

    Translate ch03-05 & ch03-06

--- ch03/ch03-05-reduction.py
+++ ch03/ch03-05-reduction.py
@@ -1,25 +1,25 @@
 # ---------------------------------
-# データ等の準備
+# Prepare the data etc.
 # ----------------------------------
 import numpy as np
 import pandas as pd
 
-# train_xは学習データ、train_yは目的変数、test_xはテストデータ
-# pandasのDataFrame, Seriesで保持します。（numpyのarrayで保持することもあります）
+# train_x is the training data, with train_y target values, and test_x is the test data
+# Saving in pandas DataFrame and Series objects. (Also possible to use numpy arrays)
 
 train = pd.read_csv('../input/sample-data/train_preprocessed_onehot.csv')
 train_x = train.drop(['target'], axis=1)
 train_y = train['target']
 test_x = pd.read_csv('../input/sample-data/test_preprocessed_onehot.csv')
 
-# 説明用に学習データとテストデータの元の状態を保存しておく
+# For explanations save the original forms of the training and test data
 train_x_saved = train_x.copy()
 test_x_saved = test_x.copy()
 
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
 
-# 標準化を行った学習データとテストデータを返す関数
+# Function to return standardized versions of the original training and test data
 def load_standarized_data():
     train_x, test_x = train_x_saved.copy(), test_x_saved.copy()
 
@@ -30,11 +30,11 @@ def load_standarized_data():
     return pd.DataFrame(train_x), pd.DataFrame(test_x)
 
 
-# MinMaxスケーリングを行った学習データとテストデータを返す関数
+# Function to return MinMax scaled versions of the original training and test data
 def load_minmax_scaled_data():
     train_x, test_x = train_x_saved.copy(), test_x_saved.copy()
 
-    # Min-Max Scalingを行う
+    # Apply Min-Max Scaling
     scaler = MinMaxScaler()
     scaler.fit(pd.concat([train_x, test_x], axis=0))
     train_x = scaler.transform(train_x)
@@ -46,144 +46,144 @@ def load_minmax_scaled_data():
 # -----------------------------------
 # PCA
 # -----------------------------------
-# 標準化されたデータを用いる
+# Use the standardized data
 train_x, test_x = load_standarized_data()
 # -----------------------------------
 # PCA
 from sklearn.decomposition import PCA
 
-# データは標準化などのスケールを揃える前処理が行われているものとする
+# Assume that the data has been preprocessed e.g. by standardization to make the scale uniform
 
-# 学習データに基づいてPCAによる変換を定義
+# Fit the PCA transformation using the training data
 pca = PCA(n_components=5)
 pca.fit(train_x)
 
-# 変換の適用
+# Apply the transformation
 train_x = pca.transform(train_x)
 test_x = pca.transform(test_x)
 
 # -----------------------------------
-# 標準化されたデータを用いる
+# Use the standardized data
 train_x, test_x = load_standarized_data()
 # -----------------------------------
 # TruncatedSVD
 from sklearn.decomposition import TruncatedSVD
 
-# データは標準化などのスケールを揃える前処理が行われているものとする
+# Assume that the data has been preprocessed e.g. by standardization to make the scale uniform
 
-# 学習データに基づいてSVDによる変換を定義
+# Fit the SVD transformation using the training data
 svd = TruncatedSVD(n_components=5, random_state=71)
 svd.fit(train_x)
 
-# 変換の適用
+# Apply the transformation
 train_x = svd.transform(train_x)
 test_x = svd.transform(test_x)
 
 # -----------------------------------
 # NMF
 # -----------------------------------
-# 非負の値とするため、MinMaxスケーリングを行ったデータを用いる
+# So that the data are non-negative, use the MinMax scaled data
 train_x, test_x = load_minmax_scaled_data()
 # -----------------------------------
 from sklearn.decomposition import NMF
 
-# データは非負の値から構成されているとする
+# Assume the data only contains non-negative values
 
-# 学習データに基づいてNMFによる変換を定義
+# Fit the NMF transformation using the training data
 model = NMF(n_components=5, init='random', random_state=71)
 model.fit(train_x)
 
-# 変換の適用
+# Apply the transformation
 train_x = model.transform(train_x)
 test_x = model.transform(test_x)
 
 # -----------------------------------
 # LatentDirichletAllocation
 # -----------------------------------
-# MinMaxスケーリングを行ったデータを用いる
-# カウント行列ではないが、非負の値であれば計算は可能
+# Use the MinMax scaled data
+# Although this is not a matrix of counts, as the values are all non-negative it is still possible to calculate
 train_x, test_x = load_minmax_scaled_data()
 # -----------------------------------
 from sklearn.decomposition import LatentDirichletAllocation
 
-# データは単語文書のカウント行列などとする
+# Assume the data is a matrix of counts of words in a document
 
-# 学習データに基づいてLDAによる変換を定義
+# Fit the Latent Dirichlet Allocation transformation using the training data
 model = LatentDirichletAllocation(n_components=5, random_state=71)
 model.fit(train_x)
 
-# 変換の適用
+# Apply the transformation
 train_x = model.transform(train_x)
 test_x = model.transform(test_x)
 
 # -----------------------------------
 # LinearDiscriminantAnalysis
 # -----------------------------------
-# 標準化されたデータを用いる
+# Use the standardized data
 train_x, test_x = load_standarized_data()
 # -----------------------------------
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 
-# データは標準化などのスケールを揃える前処理が行われているものとする
+# Assume that the data has been preprocessed e.g. by standardization to make the scale uniform
 
-# 学習データに基づいてLDAによる変換を定義
+# Fit the Linear Discriminant Analysis transformation using the training data
 lda = LDA(n_components=1)
 lda.fit(train_x, train_y)
 
-# 変換の適用
+# Apply the transformation
 train_x = lda.transform(train_x)
 test_x = lda.transform(test_x)
 
 # -----------------------------------
 # t-sne
 # -----------------------------------
-# 標準化されたデータを用いる
+# Use the standardized data
 train_x, test_x = load_standarized_data()
 # -----------------------------------
 import bhtsne
 
-# データは標準化などのスケールを揃える前処理が行われているものとする
+# Assume that the data has been preprocessed e.g. by standardization to make the scale uniform
 
-# t-sneによる変換
+# Transform using t-sne
 data = pd.concat([train_x, test_x])
 embedded = bhtsne.tsne(data.astype(np.float64), dimensions=2, rand_seed=71)
 
 # -----------------------------------
 # UMAP
 # -----------------------------------
-# 標準化されたデータを用いる
+# Use the standardized data
 train_x, test_x = load_standarized_data()
 # -----------------------------------
 import umap
 
-# データは標準化などのスケールを揃える前処理が行われているものとする
+# Assume that the data has been preprocessed e.g. by standardization to make the scale uniform
 
-# 学習データに基づいてUMAPによる変換を定義
+# Fit the UMAP transformation using the training data
 um = umap.UMAP()
 um.fit(train_x)
 
-# 変換の適用
+# Apply the transformation
 train_x = um.transform(train_x)
 test_x = um.transform(test_x)
 
 # -----------------------------------
-# クラスタリング
+# Clustering
 # -----------------------------------
-# 標準化されたデータを用いる
+# Use the standardized data
 train_x, test_x = load_standarized_data()
 # -----------------------------------
 from sklearn.cluster import MiniBatchKMeans
 
-# データは標準化などのスケールを揃える前処理が行われているものとする
+# Assume that the data has been preprocessed e.g. by standardization to make the scale uniform
 
-# 学習データに基づいてMini-Batch K-Meansによる変換を定義
+#  Fit the Mini-Batch K-Means using the training data
 kmeans = MiniBatchKMeans(n_clusters=10, random_state=71)
 kmeans.fit(train_x)
 
-# 属するクラスタを出力する
+# Output the clusters to which each class belongs
 train_clusters = kmeans.predict(train_x)
 test_clusters = kmeans.predict(test_x)
 
-# 各クラスタの中心までの距離を出力する
+# Output the distance to the center for each cluster
 train_distances = kmeans.transform(train_x)
 test_distances = kmeans.transform(test_x)
